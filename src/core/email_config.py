5e40be332752bbdcea1cb0da18cb6db823ab37cb
munyajunior <54799560+Munyajunior@users.noreply.github.com>
2025-10-31 20:22:25 +0100

    🐛 fix(email): update resend api key
    - update resend api key due to the previous one expiring

--- src/core/email_config.py
+++ src/core/email_config.py
@@ -9,7 +9,7 @@ load_dotenv()
 class EmailSettings(BaseSettings):
     """Email configuration settings"""
 
-    RESEND_API_KEY: str = "re_6bb4txSc_9XSA5K74U3W2Ktry8BnA7yJC"
+    RESEND_API_KEY: str = "re_EaW98Bgf_52R1dJDc2JfK2ahPnVqLzdZs"
     FROM_EMAIL: EmailStr = "noreply@kwantabit.com"
     FROM_NAME: str = "Dental Clinic Management System"
     SUPPORT_EMAIL: EmailStr = "support@kwantabit.com"
