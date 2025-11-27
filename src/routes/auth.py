c19525bea654b5883de708bebaff61f7f90d9266
munyajunior <54799560+Munyajunior@users.noreply.github.com>
2025-11-28 00:03:02 +0100

    ♻️ refactor(auth): simplify user registration process - remove create_default_user parameter when registering a new user

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -137,7 +137,6 @@ async def register(
         user_data=user_data,
         background_tasks=background_tasks,
         tenant_id=current_user.tenant_id,
-        create_default_user=False,
     )
     return UserPublic.from_orm_safe(user)
 
